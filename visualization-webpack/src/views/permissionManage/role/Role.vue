ebd120d948ba1061e4df248895ee0eb22cfd3dfc
Administrator <1170212285@qq.com>
2019-09-18 22:09:35 +0800

    2

--- visualization-webpack/src/views/permissionManage/role/Role.vue
+++ visualization-webpack/src/views/permissionManage/role/Role.vue
@@ -1,225 +1,74 @@
+<style scoped>
+  h2{
+    text-align: center;
+    padding: 30px;
+    font-size: 18px;
+  }
+  #chart_example{
+    width: 50%;
+    height: 500px;
+    border: 1px solid red;
+    margin: 0 auto;
+  }
+</style>
 <template>
-  <div class="role-list">
-    <ToolBar>
-      <el-button type="primary" icon="el-icon-plus" size="small" @click="editRole(false)">添加</el-button>
-      <div style="float: right">
-        <el-input
-            placeholder="请输入角色名称！"
-            size="small"
-            style="width: 140px"
-            v-model="params.name"
-            @clear="searchRole"
-            clearable>
-        </el-input>
-        <el-button @click="searchRole" type="success" icon="el-icon-search" size="small"></el-button>
-      </div>
-    </ToolBar>
-    <el-table
-        :data="users"
-        border
-        ref="table"
-        style="width: 100%">
-      <el-table-column
-          prop="id"
-          label="角色key">
-      </el-table-column>
-      <el-table-column
-          prop="username"
-          label="角色名称">
-      </el-table-column>
-      <el-table-column
-          prop="email"
-          label="描述">
-      </el-table-column>
-      <el-table-column
-          label="操作"
-          :render-header="tableAction"
-          width="120">
-        <template slot-scope="scope">
-          <el-button @click="editRole(scope.row)" type="primary" icon="el-icon-edit" size="small" circle></el-button>
-          <el-button @click="currentEditPermissions = scope.row" type="success" icon="el-icon-setting" size="small"
-                     circle></el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <RoleEdit
-        :title="roleEditTitle"
-        :dialogFormVisible="dialogFormVisible"
-        :data="currentEditRole"
-        @val-change="roleEditChange"
-        @cancel="dialogFormVisible = false"
-    >
-    </RoleEdit>
-    <PermissionsEdit
-        :role="currentEditPermissions"
-        @success="currentEditPermissions = false;"
-        @cancel="currentEditPermissions = false;"
-    ></PermissionsEdit>
-	
-	<!-- <el-pagination
-	  background
-	  layout="prev, pager, next"
-	  :total="1000">
-	</el-pagination> -->
-	<!-- @current-change="" -->
-	<el-pagination
-    @current-change="cPage"
-    :current-page="roles.currentPage"
-	  background
-	  :page-size="roles.pageNum"
-	  :pager-count="4"
-	  layout="prev, pager, next"
-	  :total="roles.totalNum">
-	</el-pagination>
-	
-	
+  <div>
+    <h2>vue中插入Echarts示例</h2>
+    <div id="chart_example">
+ 
+    </div>
   </div>
 </template>
-
+ 
 <script>
-  import request from '../../../utils/request.js'
-  import ToolBar from '@/components/ToolBar.vue';
-  import HelpHint from '@/components/HelpHint.vue';
-  import RoleEdit from './RoleEdit.vue'
-  import PermissionsEdit from './PermissionsEdit.vue'
-  import {role as roleApi} from '../../../api/role'
+  import echarts from 'echarts'
   export default {
     data() {
-      return {
-        roleEditTitle: '角色编辑',
-        currentEditRole: false,
-        dialogFormVisible: false,
-        currentEditPermissions: false,
-        params: {
-          name: '',
+      return {}
+    },
+    mounted() {
+      let this_ = this;
+      let myChart = echarts.init(document.getElementById('chart_example'));
+      let option = {
+        color: ['#f44'],
+        tooltip : {
+          trigger: 'axis',
+          axisPointer : {
+            type : 'shadow'
+          }
         },
-        // roleData: [
-        //   {id: 1, key: 'SuperAdmin', name: '超级管理员', description: '具有最高权限,全站唯一'},
-        //   {id: 2, key: 'NormalAdmin', name: '普通管理员', description: '拥有所有权限,除权限管理权限'},
-        //   {id: 4, key: 'Admin', name: '一般管理员', description: '只有查看权限'},
-        // ]
-		users:[]
-      }
-    },
-	mounted() {
-		this.getData()
-	},
-    methods: {
-		getData() {
-			request({
-				url: '/user/list',
-				method:'get'
-				
-			}).then(resp=>{
-				consol.log(resp)
-				const datas = resp.data
-				
-					this.users = datas
-					console.log(this.roles)
-				
-			}).catch(error=>{
-				//请求异常
-				console.log(error)
-			})
-		},
-		cPage(val) {
-			console.log(`当前页: ${val}`);
-			request({
-				url: '/role/index?currentPage='+`${val}`,
-				method:'post'
-				
-			}).then(resp=>{
-				const datas = resp.data
-				if(resp.status == 200) {
-					this.roles = datas
-					console.log(this.roles)
-				}
-			}).catch(error=>{
-				//请求异常
-				console.log(error)
-			})
-		  },
-      searchRole() {
-        let tableRow = this.$refs.table.$el.querySelectorAll('tbody tr');
-        let tableRowHeight = tableRow[1].offsetHeight;
-        let isjump = false;
-        for (let i = 0; i < this.roleData.length; i++) {
-          if (this.params.name && this.roleData[i].name.indexOf(this.params.name) != -1) {
-            tableRow[i].style.backgroundColor = '#85ce61';
-            if (!isjump) {
-              scrollTo(0, i * tableRowHeight + 66);
-              isjump = true;
+        xAxis : [
+          {
+            type : 'category',
+            data : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月',],
+            axisTick: {
+              alignWithLabel: true
             }
-          } else {
-            tableRow[i].style.backgroundColor = '#fff';
           }
-        }
-      },
-      tableAction() {
-        return this.$createElement('HelpHint', {
-          props: {
-            content: '编辑角色 / 编辑角色权限'
+        ],
+        yAxis : [
+          {
+            type : 'value'
           }
-        }, '操作');
-      },
-      roleEditChange(data) {
-        console.log(data);
-        let self = this;
-
-        self.$notify({
-          title: self.roleEditTitle + '成功！',
-          message: '自己对接Api,并调用你的初始化列表函数！',
-          type: 'success'
-        });
-        self.dialogFormVisible = false;
-        // self.$Api.editRole(data,r=>{
-        //   self.$message.success(self.roleEditTitle+'成功！');
-        //   self.init();
-        //   self.dialogFormVisible = false;
-        // });
-      },
-      editRole(data) {
-        if (data) {
-          this.currentEditRole = data;
-        } else {
-          this.currentEditRole = false;
-        }
-
-        this.dialogFormVisible = true;
-
-      },
-      UploadRole(data) {
-
-      },
-      deleteRole(id) {
-
-        this.$message({
-          message: '这里请求api删除或者恢复用户之后刷新分页组件，列表自动更新',
-          type: 'success'
-        });
-
-      },
-      resetting(id) {
-
-        let dom = this.$refs[id].$el;
-        dom.style.transform = 'rotate(180deg)';
-        setTimeout(() => {
-          dom.style.transform = 'rotate(0deg)'
-        }, 600)
-        this.$message({
-          message: '已经成功重置密码',
-          type: 'success'
-        });
-
-      },
-
-
+        ],
+        series : [
+          {
+            name:'每月花费',
+            type:'bar',
+            barWidth: '60%',
+            data:[995,666,444,858,654,236,645,546,846,225,547,356]
+          }
+        ]
+      };
+      myChart.setOption(option);
+ 
+      //建议加上以下这一行代码，不加的效果图如下（当浏览器窗口缩小的时候）。超过了div的界限（红色边框）
+      window.addEventListener('resize',function() {myChart.resize()});
     },
-    components: {
-      ToolBar, HelpHint, RoleEdit, PermissionsEdit
+    methods: {},
+    watch: {},
+    created() {
+ 
     }
   }
 </script>
-<style lang="scss">
-
-</style>
