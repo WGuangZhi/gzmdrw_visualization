af30abd6420a2d85005ae5d3a8d051916eb40036
Administrator <1170212285@qq.com>
2019-09-18 20:36:34 +0800

    12

--- visualization-webpack/src/views/home/Index.vue
+++ visualization-webpack/src/views/home/Index.vue
@@ -1,363 +1,88 @@
 <template>
-  <div>
-    <el-card style="border-radius: 0">
-      <div slot="header">
-        <span>概况</span>
-      </div>
-      <div class="demo">
-        <el-row :gutter="20">
-          <el-col :span="6">
-            <div class="grid-content ">
-              <div style="padding: 10px;background: #673AB7">
-                <i class="el-icon-location-outline index-icon"></i>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos earum enim fugiat illum iusto minus modi nam natus nisi sequi!
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="6">
-            <div class="grid-content ">
-              <div style="padding: 10px;background: #3c8dbc">
-                <i class="el-icon-picture index-icon"></i>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos earum enim fugiat illum iusto minus modi nam natus nisi sequi!
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="6">
-            <div class="grid-content ">
-              <div style="padding: 10px;background: #009688">
-                <i class="el-icon-service index-icon"></i>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos earum enim fugiat illum iusto minus modi nam natus nisi sequi!
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="6">
-            <div class="grid-content ">
-              <div style="padding: 10px;background: #607D8B">
-                <i class="el-icon-bell index-icon"></i>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos earum enim fugiat illum iusto minus modi nam natus nisi sequi!
-              </div>
-            </div>
-          </el-col>
-        </el-row>
-      </div>
-    </el-card>
-    <br>
-    <el-card style="border-radius: 0">
-      <div slot="header">
-        <span>Element ui组件</span>
-      </div>
-
-
-
-      <el-row>
-        <el-col :span="14">
-          <el-cascader
-              expand-trigger="hover"
-              :options="options"
-              v-model="selectedOptions">
-          </el-cascader>
-          <el-date-picker
-              v-model="dateVal"
-              type="date"
-              placeholder="选择日期">
-          </el-date-picker>
-          <el-input-number v-model="num"  :min="1" :max="10" label="计数器max=10,min=1"></el-input-number>
-          <div style="display: inline-block">
-            <el-rate
-                v-model="elRate"
-                show-score
-                :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
-            </el-rate>
-          </div>
-          <div style="margin-left: 40px;display: inline-block">
-            <el-radio v-model="radio" label="1">备选项</el-radio>
-            <el-radio v-model="radio" label="2">备选项</el-radio>
-          </div>
-          <br><br><br>
-          <div style="display: inline-block">
-            <el-steps :active="2">
-              <el-step title="步骤 1" description="这是一段很长很长很长的描述性文字"></el-step>
-              <el-step title="步骤 2" description="这是一段很长很长很长的描述性文字"></el-step>
-              <el-step title="步骤 3" description="这段就没那么长了"></el-step>
-            </el-steps>
-          </div>
-          <el-pagination
-              style="display: inline-block"
-              background
-              layout="prev, pager, next"
-              :total="100">
-          </el-pagination>
-          <br><br><br>
-          <el-checkbox-group v-model="checkList">
-            <el-checkbox label="复选框 A"></el-checkbox>
-            <el-checkbox label="复选框 B"></el-checkbox>
-            <el-checkbox label="复选框 C"></el-checkbox>
-            <el-checkbox label="禁用" disabled></el-checkbox>
-            <el-checkbox label="选中且禁用" disabled></el-checkbox>
-          </el-checkbox-group>
-
-        </el-col>
-        <el-col :span="10">
-          <div>
-            <el-button icon="el-icon-search" circle></el-button>
-            <el-button type="primary" icon="el-icon-edit" circle></el-button>
-            <el-button type="success" icon="el-icon-check" circle></el-button>
-            <el-button type="info" icon="el-icon-message" circle></el-button>
-            <el-button type="warning" icon="el-icon-star-off" circle></el-button>
-            <el-button type="danger" icon="el-icon-delete" circle></el-button>
-          </div>
-          <div style="margin-top: 40px">
-            <el-button>默认按钮</el-button>
-            <el-button type="primary">主要按钮</el-button>
-            <el-button type="success">成功按钮</el-button>
-            <el-button type="info">信息按钮</el-button>
-            <el-button type="warning">警告按钮</el-button>
-            <el-button type="danger">危险按钮</el-button>
-          </div>
-        </el-col>
-      </el-row>
-    </el-card>
-
-  </div>
+  <!--为echarts准备一个具备大小的容器dom-->
+  <div id="main" style="width:100%;height: 400px;"></div>
 </template>
-
 <script>
-  export default {
-    data() {
-      return {
-        radio:'',
-        elRate:2,
-        selectedOptions:[],
-        num:'',
-        dateVal:'',
-        checkList: ['选中且禁用','复选框 A'],
-        options: [{
-          value: 'zhinan',
-          label: '指南',
-          children: [{
-            value: 'shejiyuanze',
-            label: '设计原则',
-            children: [{
-              value: 'yizhi',
-              label: '一致'
-            }, {
-              value: 'fankui',
-              label: '反馈'
-            }, {
-              value: 'xiaolv',
-              label: '效率'
-            }, {
-              value: 'kekong',
-              label: '可控'
-            }]
-          }, {
-            value: 'daohang',
-            label: '导航',
-            children: [{
-              value: 'cexiangdaohang',
-              label: '侧向导航'
-            }, {
-              value: 'dingbudaohang',
-              label: '顶部导航'
-            }]
-          }]
-        }, {
-          value: 'zujian',
-          label: '组件',
-          children: [{
-            value: 'basic',
-            label: 'Basic',
-            children: [{
-              value: 'layout',
-              label: 'Layout 布局'
-            }, {
-              value: 'color',
-              label: 'Color 色彩'
-            }, {
-              value: 'typography',
-              label: 'Typography 字体'
-            }, {
-              value: 'icon',
-              label: 'Icon 图标'
-            }, {
-              value: 'button',
-              label: 'Button 按钮'
-            }]
-          }, {
-            value: 'form',
-            label: 'Form',
-            children: [{
-              value: 'radio',
-              label: 'Radio 单选框'
-            }, {
-              value: 'checkbox',
-              label: 'Checkbox 多选框'
-            }, {
-              value: 'input',
-              label: 'Input 输入框'
-            }, {
-              value: 'input-number',
-              label: 'InputNumber 计数器'
-            }, {
-              value: 'select',
-              label: 'Select 选择器'
-            }, {
-              value: 'cascader',
-              label: 'Cascader 级联选择器'
-            }, {
-              value: 'switch',
-              label: 'Switch 开关'
-            }, {
-              value: 'slider',
-              label: 'Slider 滑块'
-            }, {
-              value: 'time-picker',
-              label: 'TimePicker 时间选择器'
-            }, {
-              value: 'date-picker',
-              label: 'DatePicker 日期选择器'
-            }, {
-              value: 'datetime-picker',
-              label: 'DateTimePicker 日期时间选择器'
-            }, {
-              value: 'upload',
-              label: 'Upload 上传'
-            }, {
-              value: 'rate',
-              label: 'Rate 评分'
-            }, {
-              value: 'form',
-              label: 'Form 表单'
-            }]
-          }, {
-            value: 'data',
-            label: 'Data',
-            children: [{
-              value: 'table',
-              label: 'Table 表格'
-            }, {
-              value: 'tag',
-              label: 'Tag 标签'
-            }, {
-              value: 'progress',
-              label: 'Progress 进度条'
-            }, {
-              value: 'tree',
-              label: 'Tree 树形控件'
-            }, {
-              value: 'pagination',
-              label: 'Pagination 分页'
-            }, {
-              value: 'badge',
-              label: 'Badge 标记'
-            }]
-          }, {
-            value: 'notice',
-            label: 'Notice',
-            children: [{
-              value: 'alert',
-              label: 'Alert 警告'
-            }, {
-              value: 'loading',
-              label: 'Loading 加载'
-            }, {
-              value: 'message',
-              label: 'Message 消息提示'
-            }, {
-              value: 'message-box',
-              label: 'MessageBox 弹框'
-            }, {
-              value: 'notification',
-              label: 'Notification 通知'
-            }]
-          }, {
-            value: 'navigation',
-            label: 'Navigation',
-            children: [{
-              value: 'menu',
-              label: 'NavMenu 导航菜单'
-            }, {
-              value: 'tabs',
-              label: 'Tabs 标签页'
-            }, {
-              value: 'breadcrumb',
-              label: 'Breadcrumb 面包屑'
-            }, {
-              value: 'dropdown',
-              label: 'Dropdown 下拉菜单'
-            }, {
-              value: 'steps',
-              label: 'Steps 步骤条'
-            }]
-          }, {
-            value: 'others',
-            label: 'Others',
-            children: [{
-              value: 'dialog',
-              label: 'Dialog 对话框'
-            }, {
-              value: 'tooltip',
-              label: 'Tooltip 文字提示'
-            }, {
-              value: 'popover',
-              label: 'Popover 弹出框'
-            }, {
-              value: 'card',
-              label: 'Card 卡片'
-            }, {
-              value: 'carousel',
-              label: 'Carousel 走马灯'
-            }, {
-              value: 'collapse',
-              label: 'Collapse 折叠面板'
-            }]
-          }]
-        }, {
-          value: 'ziyuan',
-          label: '资源',
-          children: [{
-            value: 'axure',
-            label: 'Axure Components'
-          }, {
-            value: 'sketch',
-            label: 'Sketch Templates'
-          }, {
-            value: 'jiaohu',
-            label: '组件交互文档'
-          }]
-        }],
-      }
-    },
-    methods: {
-      open() {
-        this.$alert('这是一段内容', '标题名称', {
-          confirmButtonText: '确定',
-          callback: action => {
-            this.$message({
-              message: '恭喜你，这是一条成功消息',
-              type: 'success'
-            });
-          }
-        });
-      }
-    },
-    mounted: function () {
-
+  import echarts from 'echarts'
+    export default {
+        name: '',
+        data () {
+            return {
+                charts: '',//拿到dom对象元素
+                opinion:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎'],//侧边栏对象数据
+                opinionData:[//图表对象数据
+                  {value:335, name:'直接访问'},
+                  {value:310, name:'邮件营销'},
+                  {value:234, name:'联盟广告'},
+                  {value:135, name:'视频广告'},
+                  {value:1548, name:'搜索引擎'}
+                ]
+            }
+        },
+        methods:{
+            drawPie(id){
+               this.charts = echarts.init(document.getElementById(id))
+               this.charts.setOption({
+				 //Echarts 的悬浮框
+                 tooltip: {
+                   trigger: 'item',
+                   formatter: '{a}<br/>{b}:{c} ({d}%)'
+                 }, 
+				 //侧边栏 图例组件,展现了不同系列的标记(symbol)，颜色和名字。可以通过点击图例控制哪些系列不显示。
+                 legend: {
+                   orient: 'vertical',//可选值有horizontal(横向)和vertical（竖向）。默认值是horizontal
+                   x: 'left',//位置
+				   data:this.opinion
+                 },
+				 //系列列表。我理解为数据列表。这里可以定义每组数据内容，以及数据的展现形式。
+                 series: [
+                   {
+                     name:'访问来源',
+                     type:'pie',
+                     /* radius:['50%','70%'], */
+                     /* avoidLabelOverlap: false, */
+                     //名字设置
+					 label: {
+                       normal: {
+                         show: true,//是否显示显示名字，false为鼠标移动过去的时候显示
+                         position: 'top'//名字的位置
+                       },
+					   //鼠标移入动态的时候显示的默认样式
+                       emphasis: {
+						 color:'green',
+                         show: true,
+                         textStyle: {
+                           fontSize: '30',
+                           fontWeight: 'blod'
+                         }
+                       }
+                     },
+					 //这里是显示线，图表到name的线的指向
+					 //echarts 饼图的指示线(labelline)
+                     labelLine: {
+                       normal: {
+                         show: true
+                       }
+                     },
+                     data:this.opinionData
+                   }
+                 ]
+               })
+            }
+        },
+      //调用
+        mounted(){
+			this.drawPie('main')
+            /* this.$nextTick(function() {
+                
+            }) */
+        }
     }
-  }
 </script>
-<style lang="scss">
-  .demo{
-    margin: 10px;
-    .grid-content {
-      border-radius: 4px;
-      overflow: hidden;
-      color: #fff;
-      background: #d3dce6;
-      .index-icon{
-        font-size: 82px;
-        color: #fff;
-      }
+<style scoped>
+    * {
+        margin: 0;
+        padding: 0;
+        list-style: none;
     }
-  }
 </style>
