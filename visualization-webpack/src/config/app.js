753c742380c70d98d0593977a5ded3a4e2e52cf7
WGuangZhi <1300847590@qq.com>
2019-09-20 09:07:54 +0800

    修改

--- visualization-webpack/src/config/app.js
+++ visualization-webpack/src/config/app.js
@@ -2,9 +2,9 @@ import localKey from './localKey'
 
 
 const devApiUrl = 'http://127.0.0.1:8679';//本地开发环境
-const buildDevApiUrl = 'http://192.168.1.92:9088';//打包开发环境
-const buildTestApiUrl = 'http://192.168.1.92:6906';//打包测试环境
-const buildProApiUrl = 'http://192.168.1.92:9088';//打包正式环境
+const buildDevApiUrl = 'http://127.0.0.1:8679';//打包开发环境
+const buildTestApiUrl = 'http://127.0.0.1:8679';//打包测试环境
+const buildProApiUrl = 'http://127.0.0.1:8679';//打包正式环境
 
 
 let useApiUrl;
